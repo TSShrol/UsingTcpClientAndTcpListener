4c579ee1d8e0c5deea96de4528e714f4fe4ed105
TSShrol <shrolts@gmail.com>
2023-04-05 20:59:16 +0300

    Add project files.

---
+++ ChatTcpListenet/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatTcpListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any,8008);
+            NetworkStream stream = null;
+            try
+            {
+                tcpListener.Start();
+                byte[] data = new byte[1024];
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    Console.WriteLine($"Client connected... {tcpClient.Client.RemoteEndPoint}");
+                    stream = tcpClient.GetStream();
+                    byte[] myReadBuffer = new byte[1024];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes= 0;
+                    while (true) {
+                        do
+                        {
+                            bytes = stream.Read(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        } while (stream.DataAvailable);
+
+                        Console.WriteLine("You received the following message : " + builder);
+
+                        string message = Encoding.Unicode.GetString(data);
+                        Console.WriteLine($"{DateTime.Now.ToLongTimeString()} :: {message} ");
+                        //stream.Flush();
+
+                        data = Encoding.Unicode.GetBytes("Hello, client");
+                        stream.Write(data);
+                        //stream.Flush();
+                        builder.Clear();
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                tcpListener.Stop();
+            }
+        }
+    }
+}
---
+++ TcpClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+
+
+            byte[] data = new byte[1024];
+            NetworkStream stream = null;
+            try
+            {
+                tcpClient.Connect("127.0.0.1",8008);
+                while (true)
+                {
+                    Console.Write("Input message>> ");
+                    var message = Console.ReadLine();
+                    //var message = "Hello, server";
+                    data = Encoding.Unicode.GetBytes(message);
+                    stream = tcpClient.GetStream();
+                    stream.Write(data);
+
+                    byte[] myReadBuffer = new byte[1024];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+
+                    Console.WriteLine("You received the following message : " + builder);
+
+                }
+            }
+            catch (SocketException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null) stream.Close();
+                tcpClient.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
